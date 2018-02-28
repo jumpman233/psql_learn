e63214c6330784eaaac5ba8a3048d0ec1fae2b3d
leizhihao <leizhihao@iqianjin.com>
2018-02-28 18:45:24 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+import psycopg2
+import datetime
+
+def q1():
+    db = psycopg2.connect("dbname=news")
+
+    c = db.cursor()
+
+    print "1. What are the most popular three articles of all time?"
+
+    print ""
+
+    c.execute('''
+        SELECT title, count(*) as count
+        FROM log, articles 
+        WHERE strpos(log.path, articles.slug) > 0 
+        GROUP BY articles.title
+        ORDER BY count DESC
+        LIMIT 3;
+        ''')
+
+    data = c.fetchall()
+
+    for i in data:
+        print "● %(name)s - %(num)d" % {'name': i[0], 'num': i[1]}
+
+    print ""
+
+    db.close()
+
+def q2():
+
+    db = psycopg2.connect("dbname=news")
+
+    print "2. Who are the most popular article authors of all time?"
+
+    print ""
+
+    c = db.cursor()
+
+    c.execute('''
+        SELECT name, count
+        FROM (SELECT articles.author, count(*) AS count
+            FROM log, articles 
+            WHERE strpos(log.path, articles.slug) > 0 
+            GROUP BY articles.author
+            ORDER BY count DESC
+            LIMIT 4) AS t 
+        LEFT JOIN authors ON t.author = authors.id;
+        ''')
+
+    data = c.fetchall()
+
+    for i in data:
+        print "● %(name)s - %(num)d" % {'name': i[0], 'num': i[1]}
+
+    print ""
+
+    db.close()
+
+def q3():
+    db = psycopg2.connect("dbname=news")
+
+    print "3. On which days did more than 1% of requests lead to errors?"
+
+    print ""
+
+    c = db.cursor()
+
+    c.execute('''
+        SELECT t.time, t.ratio
+        FROM (
+           SELECT CAST(c_4 AS float)/(c_4+c_2) AS ratio, time1 AS time 
+            FROM (SELECT count(*) AS c_2, to_char(time, 'YYYY-MM-DD') AS time1 
+                FROM log 
+                WHERE status 
+                LIKE '200%' 
+                GROUP BY time1
+                ) AS t1 
+            LEFT JOIN (SELECT count(*) AS c_4, to_char(time, 'YYYY-MM-DD') as time2 
+                FROM log 
+                WHERE status 
+                LIKE '404%' 
+                GROUP BY time2
+                ) AS t2 
+            ON t1.time1 = t2.time2
+            ORDER BY ratio DESC
+            ) AS t
+        WHERE t.ratio >= 0.01;
+        ''')
+
+    data = c.fetchall()
+
+    for i in data:
+        temp_d = i[0].split('-')
+        d = datetime.datetime(int(temp_d[0]), int(temp_d[1]), int(temp_d[2])).strftime('%a %m,%y')
+        print "● %(time)s - %(num).1f %(ratio)s errors" % {'time': d, 'num': i[1] * 100, 'ratio': '%'}
+
+    print ""
+
+    db.close()
+
+q1()
+q2()
+q3()
